272dd384030c7cc77efabfaff5b55023ebcdba73
Adam Simonini <adamsimonini@users.noreply.github.com>
2019-01-13 23:23:08 -0500

    more on bindings

---
+++ growler/src/app.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        appName: 'Growler',
+        welcome: 'Welcome my nigga,',
+        name: 'Adam',
+        once: 'never update me!',
+        myHtml: '<a href="www.sinalite.ca">SinaLite</a>',
+        imagePointer: 'images/coke.png',
+        cokeStyle: {
+            color: '#FF6A00',
+            fontSize : '5em',
+            borderBottom: '1px solid black',
+        },
+        cokeImage: {
+            maxWidth: '100px',
+        },
+        doubleBlue : 'doubleBlue',
+        boolean: 'false',
+        userInput: '',
+        textarea: '',
+        pain: [],
+    },
+}); 
--- growler/src/app.vue
+++
@@ -1,6 +0,0 @@
-export default {
-    name: 'app',
-    data: {
-        appName: 'Growler'
-    }
-}
