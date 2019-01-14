62c631b680e0b292e7c553a4a5a75692b26d788e
Adam Simonini <adamsimonini@users.noreply.github.com>
2019-01-14 17:15:05 -0500

    event handlers

--- growler/src/app.js
+++ growler/src/app.js
@@ -2,7 +2,7 @@ var app = new Vue({
     el: '#root',
     data: {
         appName: 'Growler',
-        welcome: 'Welcome my nigga,',
+        welcome: 'Welcome fam,',
         name: 'Adam',
         once: 'never update me!',
         myHtml: '<a href="www.sinalite.ca">SinaLite</a>',
@@ -18,8 +18,30 @@ var app = new Vue({
         doubleBlue : 'doubleBlue',
         boolean: 'false',
         userInput: '',
-        textarea: '',
+        textArea: '',
         pain: [],
         gender: '',
+        dropdown: 'Never',
+        trimmedInput: '',
+        numericInput: '',
+        lazyBinding: '',
     },
+    methods: {
+        fillIn: function(e){
+            // the last parameter within an event handler is the event object that triggered the event
+            // within a method, "this" is bound to the Vue instance
+            this.userInput = 'Do not attempt';
+            this.textArea = 'to adjust your TV';
+            this.pain= ['Arms', 'Legs'];
+            this.gender= "Male"
+            this.dropdown = 'Sometimes';
+            console.log("An " + e + " event was fired!")
+        },
+        captureClick: function(generation){
+            generation == 'parent' ? console.log("%c parent listener was triggered first", "color: white; font-size: 16px;") : console.log("%c child was triggered after", "color: pink");
+        },
+        bubbleClick: function(generation){
+            generation == 'parent' ? console.log("%c parent was triggered after", "color: white; font-size: 16px;") : console.log("%c child listener was triggered first", "color: yellow;");
+        },
+    }
 }); 
